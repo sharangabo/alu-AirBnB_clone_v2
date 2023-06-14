8b252856acaeb815eadddd50990de45ae4bb59eb
M-Aloysie <a.murekatet@alustudent.com>
2023-06-14 11:51:19 +0000

    alu-AirBnB_clone_v2

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
-# Install nginx if not already installed
+# bash script that sets up your web servers for the deployment of web_static
 
-if ! [ -x "$(command -v nginx)" ]; then
-  sudo apt-get update
-  sudo apt-get -y install nginx
-fi
+sudo apt-get -y update
+sudo apt-get -y install nginx
 sudo service nginx start
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/{releases/test,shared}
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
+
 sudo service nginx restart
